21dc5d2087350ebc25f9eaa74c63744ef394be4a
jakewilliams33 <jakewilliams33@hotmail.com>
2023-03-21 12:27:27 +0000

    add test data

--- db/data/development-data/comments.js
+++ db/data/development-data/comments.js
@@ -12,6 +12,30 @@ module.exports = [
     body: "you should watch catwoman",
   },
   {
+    user_id: 6,
+    post: 1,
+    author: "goth_geeza_666",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+  },
+  {
+    user_id: 6,
+    post: 2,
+    author: "goth_geeza_666",
+    body: " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ",
+  },
+  {
+    user_id: 6,
+    post: 5,
+    author: "goth_geeza_666",
+    body: "fugit, sed quia consequuntur magni dolores",
+  },
+  {
+    user_id: 6,
+    post: 7,
+    author: "goth_geeza_666",
+    body: "you should watch catwoman",
+  },
+  {
     user_id: 4,
     post: 7,
     author: "cat_man",
@@ -22,5 +46,17 @@ module.exports = [
     post: 1,
     author: "your_nan",
     body: "same, love the part where he talks",
+  },
+  {
+    user_id: 4,
+    post: 1,
+    author: "cat_man",
+    body: "psum quia dolor sit amet, consectetur, adipisci ve",
+  },
+  {
+    user_id: 6,
+    post: 1,
+    author: "goth_geeza_666",
+    body: "how all this mistaken idea of denouncing pleasure an",
   },
 ];
